6a775661c68f59844a28d60b56359c9c5e8c6bb1
Samson Akanbi <samson.akanbi@outlook.com>
2020-12-20 09:15:13 +0100

    Added the core css file for additional styling.

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,33 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>All Memories</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content>
+            <ion-list>
+                <ion-item>Samson</ion-item>
+                <ion-item>Sodiq</ion-item>
+                <ion-item>Hammed</ion-item>
+                <ion-item>Ramon</ion-item>
+            </ion-list>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem } from '@ionic/vue';
+
+export default {
+    components: {
+        IonPage,
+        IonHeader,
+        IonToolbar,
+        IonTitle,
+        IonContent,
+        IonList,
+        IonItem
+    }
+}
+</script>
