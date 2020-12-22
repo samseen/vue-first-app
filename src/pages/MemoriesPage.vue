fb95e9a3837ba3201b101aa287dcb38169db999d
Samson Akanbi <samson.akanbi@outlook.com>
2020-12-22 23:38:52 +0100

    Served the memories page successfully.

--- src/pages/MemoriesPage.vue
+++ src/pages/MemoriesPage.vue
@@ -1,32 +1,21 @@
 <template>
-    <ion-page>
-        <ion-header>
-            <ion-toolbar>
-                <ion-title>All Memories</ion-title>
-            </ion-toolbar>
-        </ion-header>
-        <ion-content>
-            <ion-list>
-                <ion-item>A trip into the mountains</ion-item>
-                <ion-item>Watching Apocalypto</ion-item>
-                <ion-item>Going for Adventure</ion-item>
-            </ion-list>
-        </ion-content>
-    </ion-page>
+    <base-layout page-title="All Memories">
+        <ion-list>
+            <ion-item>A trip into the mountains</ion-item>
+            <ion-item>Watching Apocalypto</ion-item>
+            <ion-item>Going for Adventure</ion-item>
+        </ion-list>
+    </base-layout>
 </template>
 
 <script>
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem } from '@ionic/vue';
+import { IonList, IonItem } from '@ionic/vue';
+// import BaseLayout from '../components/base/BaseLayout.vue';
 
 export default {
     components: {
-        IonPage,
-        IonHeader,
-        IonToolbar,
-        IonTitle,
-        IonContent,
-        IonList,
-        IonItem
+        IonItem,
+        IonList
     }
 }
 </script>
---
+++ src/pages/MemoryDetails.vue
@@ -0,0 +1,5 @@
+<template>
+    <base-layout page-title="The Details">
+        <h2>The Details Page</h2>
+    </base-layout>
+</template>
