ad42616b94328c5ff8014ba9b393c73a99797157
Samson Akanbi <samson.akanbi@outlook.com>
2020-12-23 20:36:55 +0100

    Used state to manage the data loaded on the view.

--- src/pages/MemoriesPage.vue
+++ src/pages/MemoriesPage.vue
@@ -25,29 +25,10 @@ export default {
         IonThumbnail,
         IonLabel
     },
-    data() {
-        return {
-            memories: [
-                { 
-                    id: 'm1', 
-                    image: "https://images.unsplash.com/photo-1608382956294-8ed6afe0dfb5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
-                    title: 'A trip to the mountains', 
-                    desription: "It was a really nice trip!"
-                },
-                { 
-                    id: 'm2', 
-                    image: "https://images.unsplash.com/photo-1608585266151-854cab1a6327?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
-                    title: 'Watching Apocalypto', 
-                    desription: "Having a good family time."
-                },
-                { 
-                    id: 'm3', 
-                    image: 'https://images.unsplash.com/photo-1608608341889-6ff29021c3ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80',
-                    title: 'Going for Adventure', 
-                    desription: "Climbing the mountain with a friend."
-                },
-            ]
+    computed: {
+        memories() {
+            return this.$store.getters.memories;
         }
     }
-}
+};
 </script>
