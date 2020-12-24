309ddd75dc5679937448298bfb53d1ff2a43c750
Samson Akanbi <samson.akanbi@outlook.com>
2020-12-24 01:12:25 +0100

    Loading the title data dynamically for each details page.

--- src/pages/MemoryDetails.vue
+++ src/pages/MemoryDetails.vue
@@ -1,11 +1,26 @@
 <template>
-    <base-layout page-title="The Details" page-default-back-link="/memories">
-        <h2>The Details Page</h2>
+    <base-layout :page-title="loadedMemory ? loadedMemory.title : 'Loading...'" page-default-back-link="/memories">
+        <h2 v-if="!loadedMemory">Could not find a memory for the given id.</h2>
+        <h2 v-else>Loaded It</h2>
     </base-layout>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            memoryId: this.$route.params.id
+        }
+    },
+    computed: {
+        loadedMemory() {
+            return this.$store.getters.memory(this.memoryId);
+        }
+    },
+    // watch: {
+    //     '$route'(currentRoute) {
+    //         this.memoryId = currentRoute.params.id
+    //     }
+    // }
 }
 </script>
