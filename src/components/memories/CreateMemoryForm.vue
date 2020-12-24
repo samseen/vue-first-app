1ee7d8a5bd3c606f4e8e5dc7d780133189be50e3
Samson Akanbi <samson.akanbi@outlook.com>
2020-12-24 22:01:26 +0100

    Saved the memory details and it appears on the details page.

--- src/components/memories/CreateMemoryForm.vue
+++ src/components/memories/CreateMemoryForm.vue
@@ -1,20 +1,20 @@
 <template>
-    <form class="ion-padding">
+    <form class="ion-padding" @submit.prevent="submitForm">
         <ion-list>
             <ion-item>
                 <ion-label position="floating">Title</ion-label>
-                <ion-input type="text" required />
+                <ion-input type="text" required v-model="enteredTitle" />
             </ion-item>
             <ion-item>
                 <ion-label position="floating">Image URL</ion-label>
-                <ion-input type="url" required />
+                <ion-input type="url" required v-model="enteredImageUrl" />
             </ion-item>
             <ion-item>
                 <ion-label position="floating">Description</ion-label>
-                <ion-textarea rows="5"></ion-textarea>
+                <ion-textarea rows="5" v-model="enteredDescription"></ion-textarea>
             </ion-item>
         </ion-list>
-        <ion-button expand="full">Save</ion-button>
+        <ion-button type="submit" expand="full">Save</ion-button>
     </form>
 </template>
 
@@ -22,6 +22,7 @@
 import { IonList, IonItem, IonLabel, IonInput, IonTextarea, IonButton } from "@ionic/vue";
 
 export default {
+    emit: ['save-memory'],
     components: {
         IonList,
         IonItem,
@@ -29,6 +30,24 @@ export default {
         IonInput, 
         IonTextarea,
         IonButton
+    },
+    data() {
+        return {
+            enteredTitle: '',
+            enteredImageUrl: '',
+            enteredDescription: ''
+        };
+    },
+    methods: {
+        submitForm() {
+            const memoryData = {
+                title: this.enteredTitle,
+                imageUrl: this.enteredImageUrl,
+                description: this.enteredDescription
+            };
+            this.$emit('save-memory', memoryData);
+            this.$router.replace('/memories');
+        }
     }
 }
 </script>
--- src/pages/AddMemoryPage.vue
+++ src/pages/AddMemoryPage.vue
@@ -1,7 +1,7 @@
 <template>
     <base-layout page-title="Add a Memory" page-default-back-link="/memories">
-        <create-memory-form>
-            
+        <create-memory-form @save-memory="saveMemory">
+
         </create-memory-form>
     </base-layout>
 </template>
@@ -11,6 +11,11 @@ import CreateMemoryForm from '../components/memories/CreateMemoryForm.vue';
 export default {
     components: {
         CreateMemoryForm
+    },
+    methods: {
+        saveMemory(memoryData) {
+            this.$store.dispatch('addMemory', memoryData);
+        }
     }
 }
 </script>
