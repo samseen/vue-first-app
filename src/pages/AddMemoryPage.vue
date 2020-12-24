a6e238c9890fbca43ae28b4bea0919710e61b913
Samson Akanbi <samson.akanbi@outlook.com>
2020-12-24 17:58:22 +0100

    Separated the CreateMemoryPage as a component.

--- src/pages/AddMemoryPage.vue
+++ src/pages/AddMemoryPage.vue
@@ -1,36 +1,16 @@
 <template>
     <base-layout page-title="Add a Memory" page-default-back-link="/memories">
-        <form class="ion-padding">
-            <ion-list>
-                <ion-item>
-                    <ion-label position="floating">Title</ion-label>
-                    <ion-input type="text" required />
-                </ion-item>
-                <ion-item>
-                    <ion-label position="floating">Image URL</ion-label>
-                    <ion-input type="url" required />
-                </ion-item>
-                <ion-item>
-                    <ion-label position="floating">Description</ion-label>
-                    <ion-textarea rows="5"></ion-textarea>
-                </ion-item>
-            </ion-list>
-            <ion-button expand="full">Save</ion-button>
-        </form>
-        
+        <create-memory-form>
+            
+        </create-memory-form>
     </base-layout>
 </template>
 
 <script>
-import { IonList, IonItem, IonLabel, IonInput, IonTextarea, IonButton } from "@ionic/vue";
+import CreateMemoryForm from '../components/memories/CreateMemoryForm.vue';
 export default {
     components: {
-        IonList,
-        IonItem,
-        IonLabel,
-        IonInput, 
-        IonTextarea,
-        IonButton
+        CreateMemoryForm
     }
 }
 </script>
