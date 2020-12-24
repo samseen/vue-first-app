3ee61f2192318629d4878e76c3f7da97647cbac9
Samson Akanbi <samson.akanbi@outlook.com>
2020-12-24 12:42:59 +0100

    Created the Memory Details page component.

---
+++ src/components/memories/MemoryOverview.vue
@@ -0,0 +1,16 @@
+<template>
+    <ion-img src="" alt=""></ion-img>
+    <h2></h2>
+    <p></p>
+</template>
+
+<script>
+import { IonImg } from '@ionic/vue';
+
+export default {
+    props:
+    components: {
+        IonImg
+    }
+}
+</script>
