8c8c6d819c95fbc1ca8b4d6479e6982d25f1a824
Samson Akanbi <samson.akanbi@outlook.com>
2020-12-24 17:37:34 +0100

    Built the AddMemoryPage form.

--- src/pages/AddMemoryPage.vue
+++ src/pages/AddMemoryPage.vue
@@ -1,31 +1,36 @@
 <template>
     <base-layout page-title="Add a Memory" page-default-back-link="/memories">
-        <ion-list>
-            <ion-item>
-                <ion-label position="floating">Title</ion-label>
-                <ion-input type="text" required />
-            </ion-item>
-            <ion-item>
-                <ion-label position="floating">Image URL</ion-label>
-                <ion-input type="url" required />
-            </ion-item>
-            <ion-item>
-                <ion-label position="floating">Description</ion-label>
-                <ion-textarea rows="5"></ion-textarea>
-            </ion-item>
-        </ion-list>
+        <form class="ion-padding">
+            <ion-list>
+                <ion-item>
+                    <ion-label position="floating">Title</ion-label>
+                    <ion-input type="text" required />
+                </ion-item>
+                <ion-item>
+                    <ion-label position="floating">Image URL</ion-label>
+                    <ion-input type="url" required />
+                </ion-item>
+                <ion-item>
+                    <ion-label position="floating">Description</ion-label>
+                    <ion-textarea rows="5"></ion-textarea>
+                </ion-item>
+            </ion-list>
+            <ion-button expand="full">Save</ion-button>
+        </form>
+        
     </base-layout>
 </template>
 
 <script>
-import { IonList, IonItem, IonLabel, IonInput, IonTextarea } from "@ionic/vue";
+import { IonList, IonItem, IonLabel, IonInput, IonTextarea, IonButton } from "@ionic/vue";
 export default {
     components: {
         IonList,
         IonItem,
         IonLabel,
         IonInput, 
-        IonTextarea
+        IonTextarea,
+        IonButton
     }
 }
 </script>
